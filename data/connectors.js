1f8cdaecfdef0580f8a9f21b037efcd7edfedd48
Markus Hederström <markus.hederstrom@humany.com>
2019-04-18 08:47:51 +0200

    ssl

--- data/connectors.js
+++ data/connectors.js
@@ -16,6 +16,7 @@ import { NewEggCrawler } from 'grabr-crawler';
 // });
 
 const db = new Sequelize(process.env.DATABASE_URL, {
+  ssl: true,
   // native: true,
   // Look to the next section for possible options
 });
